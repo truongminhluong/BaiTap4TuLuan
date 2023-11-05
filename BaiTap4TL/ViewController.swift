c6fd20c3eab68f409054fc480af332ed00241908
truongminhluong <truongminhluong2003@gmail.com>
2023-11-05 15:52:39 +0700

    a

--- BaiTap4TL/ViewController.swift
+++ BaiTap4TL/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        var myView = UIView()
+        myView.center = view.center
+        myView.bounds = CGRect(x: 0, y: 0, width: 150, height: 150)
+        myView.layer.cornerRadius = 75
+        myView.backgroundColor = .red.withAlphaComponent(0.5)
+        myView.layer.borderColor = UIColor.red.cgColor
+        myView.layer.borderWidth = 2
+        view.addSubview(myView)
     }
-
-
+    
+    
 }
 

d3c445a040c2df0da775798c4136df97c3c7ae98
truongminhluong <truongminhluong2003@gmail.com>
2023-11-05 15:50:56 +0700

    Initial Commit

---
+++ BaiTap4TL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaiTap4TL
+//
+//  Created by Trương Minh Lượng Jr on 05/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
